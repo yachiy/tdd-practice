de27d1ff673c0d3e23b8befc5a33815fc1c16c75
yachiy <yachiy@yachiy-no-MacBook-Pro.local>
2019-06-11 16:19:34 +0900

    リファクタリング

--- src/yachiy/ClosedRangeTest.java
+++ src/yachiy/ClosedRangeTest.java
@@ -9,34 +9,40 @@ import static org.junit.jupiter.api.Assertions.*;
 class ClosedRangeTest {
 
     @Test
-    public void 文字列として表現できること() throws Exception {
-        ClosedRange closedRange = new ClosedRange(1, 2);
+    public void 文字列として表現できること() {
+        final int min = 1;
+        final int max = 2;
+        ClosedRange closedRange = new ClosedRange(min, max);
 
-        assertEquals("[1,2]", closedRange.toString());
+        assertEquals(String.format("[%s,%s]", min, max), closedRange.toString());
     }
 
     @Nested
     class 上端点より下端点が大きい閉区間は生成できないこと {
+        private static final int MIN = 1;
+        private static final int MAX = 2;
+
         @Test
-        public void 下端点が2で上端点が1の閉区間を生成できないこと() throws IllegalArgumentException {
-            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new ClosedRange(2, 1));
-            assertEquals("2>1", exception.getMessage());
+        public void 上端点より下端点が大きい閉区間を生成できないこと() throws IllegalArgumentException {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new ClosedRange(MAX, MIN));
+            assertEquals(String.format("%s>%s", MAX, MIN), exception.getMessage());
         }
 
         @Test
-        public void 下端点が1で上端点が2の閉区間を生成できること() {
-            assertDoesNotThrow(() -> new ClosedRange(1, 2));
+        public void 上端点が下端点より大きい閉区間を生成できること() {
+            assertDoesNotThrow(() -> new ClosedRange(MIN, MAX));
         }
 
         @Test
-        public void 下端点が1で上端点も1の閉区間を生成できること() {
-            assertDoesNotThrow(() -> new ClosedRange(1, 1));
+        public void 下端点と上端点が同じ閉区間を生成できること() {
+            assertDoesNotThrow(() -> new ClosedRange(MIN, MIN));
         }
     }
 
     @Nested
     class 指定した整数を含むかどうかを判定できること {
         private ClosedRange sut;
+        // 中間点の判定をしたいのでMINとMAXは2以上離すこと
         private static final int MIN = 1;
         private static final int MAX = 3;
 
@@ -74,27 +80,30 @@ class ClosedRangeTest {
     @Nested
     class 別の閉区間と等価かどうかを判定できること {
         private ClosedRange sut;
+        private static final int MIN = 1;
+        private static final int MAX = 2;
+        private static final int RANGE = MAX - MIN;
 
         @BeforeEach
         public void 閉区間生成() {
-            sut = new ClosedRange(1, 2);
+            sut = new ClosedRange(MIN, MAX);
         }
 
         @Test
-        public void 下端点1と上端点2の閉区間は等価であると判定すること() {
-            assertTrue(sut.isEqual(new ClosedRange(1, 2)));
+        public void 下端点と上端点が一致する閉区間は等価であると判定すること() {
+            assertTrue(sut.isEqual(new ClosedRange(MIN, MAX)));
         }
 
         @Test
-        public void 下端点3と上端店4の閉区間は等価ではないと判定すること() {
-            assertFalse(sut.isEqual(new ClosedRange(3, 4)));
+        public void 下端点と上端点が違うが幅が同じ閉区間は等価ではないと判定すること() {
+            assertFalse(sut.isEqual(new ClosedRange(MAX + 1, MAX + 1 + RANGE)));
         }
     }
 
     @Nested
     class 別の閉区間が完全に含まれるかどうか判定できること {
         private ClosedRange sut;
-        // 内包判定のため、MINとMAXは2以上離すこと
+        // 内包判定のため、MINとMAXは3以上離すこと
         private static final int MIN = 1;
         private static final int MAX = 4;
 
