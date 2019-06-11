8027fd7f345745e3252911f2480fd7af3ae8e120
yachiy <yachiy@yachiy-no-MacBook-Pro.local>
2019-06-11 14:06:08 +0900

    下端点が上端点より大きい場合に閉区間を生成しないよう実装

--- src/yachiy/ClosedRange.java
+++ src/yachiy/ClosedRange.java
@@ -5,12 +5,15 @@ public class ClosedRange {
     private int upperEndpoint;
 
     public ClosedRange(int lowerEndpoint, int upperEndpoint) {
+        if (lowerEndpoint > upperEndpoint) {
+            throw new IllegalArgumentException(String.format("%s>%s", lowerEndpoint, upperEndpoint));
+        }
         this.lowerEndpoint = lowerEndpoint;
         this.upperEndpoint = upperEndpoint;
     }
 
     @Override
     public String toString() {
-        return "["+  lowerEndpoint + "," + upperEndpoint + "]" ;
+        return String.format("[%s,%s]", lowerEndpoint, upperEndpoint);
     }
 }
--- src/yachiy/ClosedRangeTest.java
+++ src/yachiy/ClosedRangeTest.java
@@ -1,15 +1,26 @@
 package yachiy;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ClosedRangeTest {
 
     @Test
-    public void 文字列として表現できる() throws Exception{
-        ClosedRange closedRange = new ClosedRange(1, 3);
+    public void 文字列として表現できる() throws Exception {
+        ClosedRange closedRange = new ClosedRange(1, 2);
 
-        assertEquals("[1,3]", closedRange.toString());
+        assertEquals("[1,2]", closedRange.toString());
+    }
+
+    @Nested
+    class 上端点より下端点が大きい閉区間を作ることはできない {
+        @Test
+        public void 下端点が2で上端点が1の閉区間を生成できないこと() throws IllegalArgumentException {
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new ClosedRange(2, 1));
+            assertEquals("2>1", exception.getMessage());
+        }
     }
 }
